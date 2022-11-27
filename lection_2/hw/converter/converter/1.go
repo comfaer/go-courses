9d7e57e3b0f96855a12d76d6f3a77641f00ab425
Maxim Ermakov <ermakov-99@bk.ru>
2022-11-27 15:15:09 +0300

    hw1

--- lection_2/hw/converter/converter/1.go
+++ lection_2/hw/converter/converter/1.go
@@ -1,2 +1,31 @@
 package converter
 
+import "strings"
+
+type converter struct {
+	symbols []string
+	values  []int
+}
+
+type Converter interface {
+	IntToRoman(number int) string
+}
+
+func (conv *converter) IntToRoman(number int) string {
+	var result strings.Builder
+
+	for i, val := range conv.values {
+		for number >= val {
+			result.WriteString(conv.symbols[i])
+			number -= val
+		}
+	}
+	return result.String()
+}
+
+func NewConverter(values []int, symbols []string) Converter {
+	return &converter{
+		values:  values,
+		symbols: symbols,
+	}
+}
